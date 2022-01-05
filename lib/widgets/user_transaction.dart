8a1738bbc379bce338811453b1207100888b6a69
Arjav Patel <patel4075@saskpolytech.ca>
2022-01-05 15:28:06 -0600

    first commit

---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+// import './new_transaction.dart';
+// import './transaction_list.dart';
+// import '../models/transaction.dart';
+
+// class UseTransaction extends StatefulWidget {
+//   @override
+//   _UseTransactionState createState() => _UseTransactionState();
+// }
+
+// class _UseTransactionState extends State<UseTransaction> {
+  
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         NewTransaction(_addNewTransaction),
+//         TransactionList(_userTransactions),
+//       ],
+//     );
+//   }
+// }
